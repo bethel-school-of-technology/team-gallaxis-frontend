9eb376e609448330dbe535ccafc75c10e27b7add
Monica Nicole Vargas <monicanicolevargas@monicas-air.home>
2021-05-10 17:42:08 -0500

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,58 @@
 import React from 'react';
 
+//import './index.css';//
+
+import { useFormik } from 'formik';
+
 const Home = () => {
+    const formik = useFormik({
+        initialValues: {
+            Id: '',
+            Name: '',
+            email: '',
+            Neighborhood: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values));
+        }
+    });
     return (
         <div>
-            <h1>Home Page</h1>
-            <h1>Welcome to Nestly</h1>
+            <h2> Welcome To NESTLY!</h2>
 
+            <form onSubmit={formik.handleSubmit}>
+                <p>
+                    <form>
+                        <label>
+                            UserName:
+    <input type="text" name="UserName" />
+                        </label>
+
+                    </form>
+                    <form>
+                        <label>
+                            Password:
+    <input type="text" name="Password" />
+                        </label>
+                        <input type="submit" value="Sign In" />
+                    </form>
+                    <form>
+                        <label>
+                            Register:
+    <input type="text" name="Register" />
+                        </label>
+                        <input type="submit" value="Register" />
+                    </form>
+                </p>
+
+            </form>
         </div>
 
-    )
-};
 
-export default Home;
+
+
+    )
+}
+
+
+export default Home
--- src/index.js
+++ src/index.js
@@ -1,63 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';//
-import { useFormik } from 'formik';
-
-const HomeComponent = () => {
-  const formik = useFormik({
-    initialValues: {
-      Id: '',
-      Name: '',
-      email: '',
-      Neighborhood: '',
-    },
-    onSubmit: values => {
-      alert(JSON.stringify(values));
-    }
-  });
-  return (
-    <div>
-      <h2> Welcome To NESTLY!</h2>
-
-      <form onSubmit={formik.handleSubmit}>
-        <p>
-          <form>
-            <label>
-              UserName:
-    <input type="text" name="UserName" />
-            </label>
-
-          </form>
-          <form>
-            <label>
-              Password:
-    <input type="text" name="Password" />
-            </label>
-            <input type="submit" value="Sign In" />
-          </form>
-          <form>
-            <label>
-              Register:
-    <input type="text" name="Register" />
-            </label>
-            <input type="submit" value="Register" />
-          </form>
-        </p>
-
-      </form>
-    </div>
-
-
-
-
-  )
-}
-
-
-
-const element = <HomeComponent></HomeComponent>
-
-ReactDOM.render(element, document.getElementById('root'));
-
-
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+reportWebVitals();

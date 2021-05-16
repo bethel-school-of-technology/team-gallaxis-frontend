c3fb618f3f28fe5cf1c56571e577f9bf214a3eae
joshuafisaac <joshuafisaac@gmail.com>
2021-05-16 06:10:58 -0400

    created navbar

--- src/components/BuySell.js
+++ src/components/BuySell.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import UserNavBar from './UserNavBar.Js';
 
 const BuySell = ({ match }) => {
     return (
         <div>
+            <UserNavBar/>
             <h3>Buy and Sell</h3>
         </div>
 

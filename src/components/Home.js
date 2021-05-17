8e7d3467d25eaceebffedb45fff7e59f201f36f4
Monica Nicole Vargas <monicanicolevargas@monicas-air.home>
2021-05-16 21:45:12 -0500

    updated funcional input slots for Homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,89 +1,33 @@
-
-import React, { useState } from 'react';
-
+import { useState } from 'react';
 
 
-const Home = () => {
-    const [values, setValues] = useState({
-        username: '',
-        email: '',
-        password: '',
-        password2: '',
+const Home = (props) => {
 
-    })
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    const handleChange = e => {
-        const { name, value } = e.target
-        setValues({
-            ...values,
-            [name]: value
-
-        })
+    function submitForm(evt) {
+        evt.preventDefault();
+        console.log("submit form is called");
+        let loginInfo = {
+            username,
+            password
+        }
+        console.log(loginInfo);
     }
 
-
     return (
-        <div className='form-content-right'>
-            <form className='form'>
-                <h1> Welcome to NESTLY where you connect with your CommUNITY</h1>
-                <div className='form-inputs'>
-                    <label htmlFor='username'
-                        className='form-label'>
-                        UserName
-                    </label>
-                    <input
-                        id='username'
-                        type='text'
-                        name='username'
-                        className='form-input'
-                        placeholder='Enter your username'
-                        value={values.username}
-                        onChange={handleChange}
-                    />
-                </div>
+        <div>
+            <form onSubmit={evt => submitForm(evt)}>
+                {/* <label for="username"></label> */}
+                <input type="text" placeholder="Enter Username" name="username" onChange={evt => setUsername(evt.target.value)}></input>
 
-
-                <div className='form-inputs'>
-                    <label htmlFor='Password'
-                        className='form-label'>
-                    </label>
-                        Password
-                        <input
-                        id='Password'
-                        type='Password'
-                        name='Password'
-                        className='form-input'
-                        placeholder='Enter your Password'
-                        value={values.password}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className='form-inputs'>
-                    <label htmlFor='email'
-                        className='form-label'>
-
-                        Confirm Password
-                    </label>
-                    <input
-                        id='Password2'
-                        type='Password'
-                        name='Password2'
-                        className='form-input'
-                        placeholder='Enter your Password'
-                        value={values.password2}
-                        onChange={handleChange}
-                    />
-                </div>
-                <button className='form-input-btn'
-                    type='submit'>
-                    Sign up!
-                </button>
-                <span className="form-input-login">
-                    Already have an account? Login <a href="#">here</a>
-                </span>
+                {/* <label for="password"></label> */}
+                <input type="text" placeholder="Enter Password" name="password" onChange={evt => setPassword(evt.target.value)}></input>
+                <button type="submit">Login!</button>
             </form>
         </div >
     )
-};
+}
 
 export default Home;

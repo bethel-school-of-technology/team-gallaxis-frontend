2033d4c4690b812ed816d79caecf2fa56023b79e
Monica Nicole Vargas <monicanicolevargas@monicas-air.home>
2021-05-16 22:08:57 -0500

    added functioning input to Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,105 +1,42 @@
-
-import React, { useState } from 'react';
-
+import { useState } from 'react';
 
 
-const Register = () => {
-    const [values, setValues] = useState({
-        username: '',
-        email: '',
-        password: '',
-        password2: '',
+const Register = (props) => {
 
-    })
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [neighborhood, setNeighborhood] = useState("");
 
+    function submitForm(evt) {
+        evt.preventDefault();
+        console.log("submit form is called");
+        let loginInfo = {
+            username,
+            password,
+            email,
+            neighborhood
 
-
-    const handleChange = e => {
-        const { name, value } = e.target
-        setValues({
-            ...values,
-            [name]: value
-
-        })
+        }
+        console.log(loginInfo);
     }
 
-
     return (
-        <div className='form-content-right'>
-            <form className='form'>
-                <h1> Get Registered Today with Nestly Neighborhood and Join the CommUNITY, Your CommUNITY </h1>
-                <div className='form-inputs'>
-                    <label htmlFor='username'
-                        className='form-label'>
-                        UserName
-                    </label>
-                    <input
-                        id='username'
-                        type='text'
-                        name='username'
-                        className='form-input'
-                        placeholder='Enter your username'
-                        value={values.username}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className='form-inputs'>
-                    <label htmlFor='email'
-                        className='form-label'>
-                        Email
-                    </label>
-                    <input
-                        id='email'
-                        type='email'
-                        name='email'
-                        className='form-input'
-                        placeholder='Enter your Email'
-                        value={values.email}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className='form-inputs'>
-                    <label htmlFor='email'
-                        className='form-label'>
+        <div>
+            <form onSubmit={evt => submitForm(evt)}>
+                {/* <label for="username"></label> */}
+                <input type="text" placeholder="Enter Username" name="username" onChange={evt => setUsername(evt.target.value)}></input>
 
-                        Password
-                    </label>
-                    <input
-                        id='Password'
-                        type='Password'
-                        name='Password'
-                        className='form-input'
-                        placeholder='Enter your Password'
-                        value={values.password}
-                        onChange={handleChange}
-                    />
-                </div>
-                <div className='form-inputs'>
-                    <label htmlFor='email'
-                        className='form-label'>
+                {/* <label for="password"></label> */}
+                <input type="text" placeholder="Enter Password" name="password" onChange={evt => setPassword(evt.target.value)}></input>
 
-                        Confirm Password
-                    </label>
-                    <input
-                        id='Password2'
-                        type='Password'
-                        name='Password2'
-                        className='form-input'
-                        placeholder='Enter your Password'
-                        value={values.password2}
-                        onChange={handleChange}
-                    />
-                </div>
-                <button className='form-input-btn'
-                    type='submit'>
-                    Sign up!
-                </button>
-                <span className="form-input-login">
-                    Already have an account? Login <a href="#">here</a>
-                </span>
+                <input type="text" placeholder="Enter Email" name="email" onChange={evt => setEmail(evt.target.value)}></input>
+
+                <input type="text" placeholder="Enter Neighborhood" name="neighborhood" onChange={evt => setNeighborhood(evt.target.value)}></input>
+                <button type="submit">Register to NESTLY!</button>
             </form>
-        </div>
+        </div >
     )
-};
+}
 
 export default Register;
